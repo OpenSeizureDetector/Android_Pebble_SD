6d4c2495b8677280f205c370c1ba2c101650b3aa
Graham Jones <grahamjones139@gmail.com>
2016-01-01 22:07:33 +0000

    modified build.gradle to return default values during testing.

--- app/src/test/java/uk/org/openseizuredetector/OsdUtilTest.java
+++ app/src/test/java/uk/org/openseizuredetector/OsdUtilTest.java
@@ -1,6 +1,13 @@
 package uk.org.openseizuredetector;
 
+import android.app.Activity;
+
+import org.junit.Before;
 import org.junit.Test;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.is;
 
 import static org.junit.Assert.*;
 
@@ -16,7 +23,11 @@ public class OsdUtilTest {
 
     @Test
     public void testStartServer() throws Exception {
-
+        //Activity a = new Activity();
+        OsdUtil util = new OsdUtil(null);
+        assertThat(util.isServerRunning(), is(true));
+        assertThat(true, is (true));
+        //assertThat(true, is(false));
     }
 
     @Test
