f40c363808685aef5894d44063657e79707f1f65
Graham Jones <grahamjones139@gmail.com>
2019-11-12 11:15:42 +0000

    Starting to add an activity to query the database

---
+++ app/src/main/java/uk/org/openseizuredetector/DBQueryActivity.java
@@ -0,0 +1,13 @@
+package uk.org.openseizuredetector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DBQueryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dbquery);
+    }
+}
