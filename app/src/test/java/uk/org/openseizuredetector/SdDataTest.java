990b9e345cb9e04e742f5b679d2ecc139d029e6e
Graham Jones <grahamjones139@gmail.com>
2022-01-23 06:29:43 +0000

    Setting up test framework for logmanager - does not do anything useful yet!

---
+++ app/src/test/java/uk/org/openseizuredetector/SdDataTest.java
@@ -0,0 +1,45 @@
+package uk.org.openseizuredetector;
+
+import android.os.Build;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.TimeZone;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = {Build.VERSION_CODES.O_MR1}, packageName = "uk.org.openseizuredetector")
+public class SdDataTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testConstructor() {
+        SdData sd = new SdData();
+        assertTrue(true);
+
+    }
+
+    public void testFromJSON() {
+    }
+
+    public void testTestToString() {
+    }
+
+    public void testToJSON() {
+    }
+
+    public void testToDataString() {
+    }
+
+    public void testToCSVString() {
+    }
+}
