480a735d507525c4aab02b39af38280394f3fd37
AroonPro <bramregtien@gmail.com>
2023-06-02 22:44:41 +0200

    Adding empty AuthenticateActivityTest

---
+++ app/src/androidTest/java/uk/org/openseizuredetector/AuthenticateActivityTest.java
@@ -0,0 +1,34 @@
+package uk.org.openseizuredetector;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuthenticateActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onStart() {
+    }
+
+    @Test
+    public void onStop() {
+    }
+
+    @Test
+    public void getAuthToken() {
+    }
+}
