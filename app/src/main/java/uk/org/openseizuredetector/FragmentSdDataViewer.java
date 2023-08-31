d2577c1e0e094c9de979414a2ddeefc8ed11c66b
Graham Jones <grahamjones139@gmail.com>
2023-08-31 21:02:00 +0100

    Added pager so we can swipe between fragments - only have two dummy fragments so far though - need to port the original mainActivity display components to separate fragments next.

--- app/src/main/java/uk/org/openseizuredetector/FragmentSdDataViewer.java
+++ app/src/main/java/uk/org/openseizuredetector/FragmentSdDataViewer.java
@@ -1,19 +1,29 @@
 package uk.org.openseizuredetector;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentSdDataViewer#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class FragmentSdDataViewer extends Fragment {
+    String TAG = "FragmentSdDataViewer";
+    Context mContext;
+    OsdUtil mUtil;
+    SdServiceConnection mConnection;
+    final Handler updateUiHandler = new Handler();
+    Timer mUiTimer;
+    protected View mRootView;
 
 
     public FragmentSdDataViewer() {
@@ -23,7 +33,14 @@ public class FragmentSdDataViewer extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Log.i(TAG,"onCreate()");
+        mContext = getContext();
+        mUtil = new OsdUtil(mContext, updateUiHandler);
+        mConnection = new SdServiceConnection(mContext);
+
+
     }
 
     @Override
@@ -32,4 +49,78 @@ public class FragmentSdDataViewer extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sd_data_viewer, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRootView = view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart()");
+        if (mUtil.isServerRunning()) {
+            Log.i(TAG, "onStart() - Binding to Server");
+            mUtil.bindToServer(mContext, mConnection);
+        } else {
+            Log.i(TAG, "onStart() - Server Not Running");
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume()");
+        mUiTimer = new Timer();
+        mUiTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                updateUiOnUiThread();
+            }
+        }, 0, 1000);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause()");
+        mUiTimer.cancel();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop()");
+        mUtil.unbindFromServer(mContext, mConnection);
+    }
+
+    /**
+     * If we don't use this .post() trick, we get errors about the wrong thread trying to
+     * update the user interface views...
+     */
+    private void updateUiOnUiThread() {
+        updateUiHandler.post(new Runnable(){
+            @Override
+            public void run() {
+                updateUi();
+            }
+        });
+    }
+
+    /**
+     * The subclasses should override this to draw their own UI.
+     */
+    protected void updateUi() {
+        Log.d(TAG,"updateUi()");
+        TextView tv;
+        tv = (TextView)mRootView.findViewById(R.id.fragment_sddata_viewer_tv1);
+        if (mConnection.mBound) {
+            tv.setText("Bound to Server");
+        } else {
+            tv.setText("****NOT BOUND TO SERVER***");
+        }
+
+    }
+
 }
--- app/src/main/java/uk/org/openseizuredetector/MainActivity2.java
+++ app/src/main/java/uk/org/openseizuredetector/MainActivity2.java
@@ -1,20 +1,72 @@
 package uk.org.openseizuredetector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private String TAG = "MainActivity2";
+    private ViewPager2 mFragmentPager;
+    private FragmentStateAdapter mFragmentStateAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(true)
-                    .add(R.id.fragment_container_view, FragmentOsdAlg.class, null)
-                    .commit();
+            // Instantiate a ViewPager2 and a PagerAdapter.
+            mFragmentPager = findViewById(R.id.fragment_pager);
+            mFragmentStateAdapter = new ScreenSlideFragmentPagerAdapter(this);
+            mFragmentPager.setAdapter(mFragmentStateAdapter);
+            //getSupportFragmentManager().beginTransaction()
+            //        .setReorderingAllowed(true)
+            //        .add(R.id.fragment_container_view, FragmentOsdAlg.class, null)
+            //        .commit();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mFragmentPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mFragmentPager.setCurrentItem(mFragmentPager.getCurrentItem() - 1);
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlideFragmentPagerAdapter extends FragmentStateAdapter {
+        private String TAG = "ScreenSlideFragmentPagerAdapter";
+        public ScreenSlideFragmentPagerAdapter(FragmentActivity fa) {
+            super(fa);
+        }
+
+        @Override
+        public Fragment createFragment(int position) {
+            switch(position) {
+                case 0:
+                    return new FragmentOsdAlg();
+                case 1:
+                    return new FragmentHrAlg();
+                default:
+                    Log.e(TAG,"createFragment() - invalid Position "+position);
+                    return null;
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return 2;
         }
     }
 }
