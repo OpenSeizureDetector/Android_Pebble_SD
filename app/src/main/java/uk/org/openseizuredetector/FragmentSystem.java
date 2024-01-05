337dadeecd06c794b54aa27e4e8d6cab82360ea3
AroonPro <bramregtien@gmail.com>
2024-01-05 22:28:39 +0100

    Corrected powerDisplay Now should  the power be correctly shown in PowerGraph

--- app/src/main/java/uk/org/openseizuredetector/FragmentSystem.java
+++ app/src/main/java/uk/org/openseizuredetector/FragmentSystem.java
@@ -221,18 +221,20 @@ public class FragmentSystem extends FragmentOsdBaseClass {
                 switchWatchGraphToPhoneGraph = mRootView.findViewById(R.id.switchToPowerGraph);
 
                 if (Objects.nonNull(mConnection.mSdServer.getLineData(switchWatchGraphToPhoneGraph.isChecked()))) {
-                    lineChartPowerLevel.clear();
-                    if (mConnection.mSdServer.getLineDataSet(switchWatchGraphToPhoneGraph.isChecked()).getYVals().size() > 0) {
+                    if (Objects.nonNull(lineChartPowerLevel)){
+                        lineChartPowerLevel.clear();
+                        if (mConnection.mSdServer.getLineDataSet(switchWatchGraphToPhoneGraph.isChecked()).getYVals().size() > 0) {
 
-                        lineChartPowerLevel = mRootView.findViewById(R.id.lineChartBattery);
+                            lineChartPowerLevel = mRootView.findViewById(R.id.lineChartBattery);
 
-                        lineChartPowerLevel.setData(mConnection.mSdServer.getLineData(switchWatchGraphToPhoneGraph.isChecked()));
+                            lineChartPowerLevel.setData(mConnection.mSdServer.getLineData(switchWatchGraphToPhoneGraph.isChecked()));
 
-                        lineChartPowerLevel.getData().notifyDataChanged();
-                        lineChartPowerLevel.notifyDataSetChanged();
-                        lineChartPowerLevel.refreshDrawableState();
-                        if (mConnection.mSdServer.mBound){
-                            lineChartPowerLevel.postInvalidate();
+                            lineChartPowerLevel.getData().notifyDataChanged();
+                            lineChartPowerLevel.notifyDataSetChanged();
+                            lineChartPowerLevel.refreshDrawableState();
+                            if (mConnection.mSdServer.mBound) {
+                                lineChartPowerLevel.postInvalidate();
+                            }
                         }
                     }
 
